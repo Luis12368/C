1e448a2b650e285421b8c027031d198f155ef1bf
Luis Gustavo Hauck <72949389+Luis12368@users.noreply.github.com>
2022-05-24 15:55:50 -0300

    Update

--- Projetos/Exercicios/exercicio003.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
-int numero;
-printf("Entre com um número inteiro:\n");
-scanf("%d", &numero);
-}
--- Projetos/Exercicios/exercicio018.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main(){
-
-int a=3, b=2, c;
-
-c = a ! = b;
-
-printf("c = %d", c);
